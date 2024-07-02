960786ff8af883eb8a89d2cec3832c47f24db05c
utsavs-icpl <utsavs@infopercept.com>
2024-07-02 14:56:04 +0530

    feat: changes in gitignore

---
+++ _site/assets/js/data/swconf.js
@@ -0,0 +1,24 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1719912300',resources: [
+      '/blogs/assets/css/jekyll-theme-chirpy.css',
+      '/blogs/',
+      
+        '/blogs/categories/',
+      
+        '/blogs/tags/',
+      
+        '/blogs/archives/',
+      
+        '/blogs/about/',
+      
+
+      
+      
+    ],denyPaths: [
+      
+    ],
+    purge: false
+  
+};
+
