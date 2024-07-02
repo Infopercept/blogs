29aced5280e425c31609d7b8a9df515d5440f3b6
utsavs-icpl <utsavs@infopercept.com>
2024-07-02 18:25:45 +0530

    feat: commit condition

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1719924787',resources: [
+    cacheName: 'chirpy-1719924924',resources: [
       '/blogs/assets/css/jekyll-theme-chirpy.css',
       '/blogs/',
       
