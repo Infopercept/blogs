f64fa18ed8f77b3439fef41e8ddeb7cf9a17766d
utsavs-icpl <utsavs@infopercept.com>
2024-07-02 18:13:37 +0530

    feat: gitignor

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1719924071',resources: [
+    cacheName: 'chirpy-1719924198',resources: [
       '/blogs/assets/css/jekyll-theme-chirpy.css',
       '/blogs/',
       
